b1734887efb683a29bf5e077f3f3cfe7c4f8bbbd
TG184568 <184568@stud.hvl.no>
2025-05-19 23:25:59 +0200

    Adding final files

---
+++ src/main/java/no/hvl/dat110/App.java
@@ -0,0 +1,7 @@
+package no.hvl.dat110;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from App in dat110-project2!");
+    }
+} 
---
+++ src/main/java/no/hvl/dat110/SystemInfo.java
@@ -0,0 +1,7 @@
+package no.hvl.dat110;
+
+public class SystemInfo {
+    public static void printInfo() {
+        System.out.println("System Info: dat110-project2");
+    }
+} 
